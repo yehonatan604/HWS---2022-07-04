0533ebf4339639e871186183fd34306198929606
yehonatan604 <97249220+yehonatan604@users.noreply.github.com>
2022-07-09 10:52:08 +0300

    test

--- scripts/script.js
+++ scripts/script.js
@@ -13,8 +13,6 @@ let messages = {
     newList: 'New List: ',
 
     exercise3: '\n----Exercise 3----\n\n',
-    aValue: 'Please Enter "A" Value',
-    bValue: 'Please Enter "B" value',
     isNumber: 'Parameters are numbers, so result will be their sum: Test function verifies equality: ',
     isNaN: 'Parameters are strings, so result will be their join: Test function verifies equality: ',
     isArray: 'Parameters are arrays, so result will be the concatenation of the values inside the arrays: Test function verifies similarity: ',
@@ -24,19 +22,24 @@ let messages = {
     exercise4: '\n----Exercise 4----\n\n',
 
     exercise5: '\n----Exercise 5----\n\n',
+
+    exercise6: '\n----Exercise 6----\n\n',
 }
 
 ///////////////////////// Exercise 1: Function Times 3
 
-var collection1 = [10, 2];
-var collection2 = [5, 0];
-var collection3 = [2, 3];
-
-function ex1(collection1, collection2, collection3) {
-    return (collection1[0] * collection2[0] * collection3[0]) + (collection1[1] * collection2[1] * collection3[1])
+function ex1(num1, num2) {
+    return function (num3, num4) {
+        return function (num5, num6) {
+            return ((num1 * num3 * num5) + (num2 * num4 * num6));
+        }
+    }
 }
 
-console.log(messages.exercise1 + ex1(collection1, collection2, collection3));
+console.log(messages.exercise1 + ex1(1, 2)(1, 1)(2, 3));
+console.log(ex1(10, 2)(5, 0)(2, 3));
+console.log(ex1(1, 2)(2, 3)(3, 4));
+console.log(ex1(1, 2)(0, 3)(3, 0));
 
 ///////////////////////// Exercise 2: Fix the Code: Un-Mutating an Array 
 
@@ -64,24 +67,23 @@ console.log(messages.exercise3);
 
 function createTest(a, b) {
     if (Array.isArray(a) && Array.isArray(b)) {
-        console.log(messages.isArray + assertSimilar(a, b));
-    }
-    else if (!isNaN(a) && !isNaN(b)) {
-        console.log(messages.isNumber + assertEquals(a, b, Number(a) + Number(b)));
+        return assertSimilar(a, b);
     }
     else if (isNaN(a) && isNaN(b)) {
-        console.log(messages.isNaN + assertEquals(a, b, a + b));
+        return assertEquals(a, b, a + b);
+    }
+    else {
+        return assertEquals(a, b, Number(a) + Number(b));
     }
 
 }
-
 function assertEquals(a, b, result) {
     return a + " + " + b + " = " + result;
 }
 
 function assertSimilar(a, b) {
-    let sumA = 0; 
-    let sumB = 0;
+    let sumA = "";
+    let sumB = "";
 
     for (let i = 0; i < a.length; i++) {
         sumA += a[i];
@@ -91,46 +93,65 @@ function assertSimilar(a, b) {
         sumB += b[i];
     }
 
-    return messages.sumA + sumA + messages.sumB + sumB;
+    return a + " + " + b + " = " + (a + " " + b);
 }
 
-createTest(1, 1);
-createTest('a', 'b');
-createTest([1, 2, 3], [4, 5, 6]);
+console.log(createTest(1, 1));
+console.log(createTest('a', 'b'));
+console.log(createTest(['s', 't', 'r'], ['i', 'n', 'g']));
 
 ///////////////////////// Exercise 4: Backspace Attack
 
 console.log(messages.exercise4);
 
-function erase(str){
-    for (let i = 0; i < str.length; i++)
-    {
-
-        if (str[i] == "#")
-        {
-            str = str.substring(0, i) + str.substring(i+1);
-            i--;
+function erase(str) {
+    var newWord = [];
+    for (var i = 0, j = 0; i < str.length; i++) {
+        if (str[i] != '#') {
+            newWord[j] = str[i];
+            j++;
+        }
+        else {
+            j--;
+            newWord[j] = '';
         }
     }
-    return str;
+    return newWord.join("");
 }
 
 console.log(erase("he##l#hel#llo"));
-console.log(erase("ab#cdef#g"));
 console.log(erase("major# spar##ks"));
 console.log(erase("si###t boy"));
-console.log(erase("####"));
 
 ///////////////////////// Exercise 5: Moving Partition
 
-function movingPartition(arr){
+console.log(messages.exercise5);
+
+function movingPartition(arr) {
     let myArr = [];
-    for (let i = 0; i < arr.length; i++)
-    {
-        myArr.push(arr.slice(0,i));
+    for (let i = 1; i < arr.length; i++) {
+        myArr.push(arr.slice(0, i));
+        myArr.push(arr.slice(i, arr.length));
     }
     return myArr;
 }
+console.log(...movingPartition([1, 2, 3, 4, 5]));
 
-movingPartition([1,2,3,4,5]).forEach( value => console.log(value));
+///////////////////////// Exercise 6: Function Factory
 
+console.log(messages.exercise6);
+
+function makePlus(num1) {
+    return function (num2) {
+        return num1 + num2;
+    }
+}
+
+const plusFive = makePlus(5)
+console.log(plusFive(2));
+console.log(plusFive(-8));
+
+const plusTen = makePlus(10)
+console.log(plusTen(0));
+console.log(plusTen(188));
+console.log(plusFive(plusTen(0)));
